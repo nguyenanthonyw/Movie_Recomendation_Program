cb6f6a08aaaa59889e62e903c7db184efd3124e9
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-09-08 18:51:33 -0500

    set up for loading data to the program

---
+++ main.py
@@ -0,0 +1,7 @@
+# movie recommendation program
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_exktraction.text import CountVectorizer
+
