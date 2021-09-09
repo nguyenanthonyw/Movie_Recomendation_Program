74c2ba8f30f8fe1cd1a62748726f3a83c7f5d516
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-09-08 19:16:10 -0500

    created a list of important columns and the shape of the data set

--- main.py
+++ main.py
@@ -3,5 +3,22 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_exktraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+#load data
+
+df = pd.read_csv('IMDB-Movie-Data.csv')
+print(df.head(3))
+
+
+
+#get count of movies in the data set and the number of columns
+df.shape
+(1000, 13)
+
+# create list of important columns for the recommendation engine
+columns = ['Actors', 'Director', 'Genre', 'Title']
+
+print(df[columns].head(3))
+
 
